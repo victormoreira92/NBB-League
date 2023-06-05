5dae4a05dbad004cf312a49dc32bedddbb8f243a
victormoreira92 <victormoreira.ti@outlook.com>
2023-06-05 13:38:36 -0300

    add metodos a classe DataNBB.

--- src/main/java/org/model/DataNbb.java
+++ src/main/java/org/model/DataNbb.java
@@ -1,4 +1,29 @@
 package org.model;
 
+import java.util.*;
+
 public class DataNbb {
+    private Set<Team> teams;
+    private List<Game> games;
+
+    public DataNbb() {
+        this.teams = new LinkedHashSet<Team>();
+        this.games = new ArrayList<Game>();
+    }
+
+    public void addTeam(Team team) {
+        this.teams.add(team);
+    }
+
+    public void addGame( Game game) {
+        this.games.add(game);
+    }
+
+    public Set<Team> getTeams() {
+        return Collections.unmodifiableSet(teams);
+    }
+
+    public List<Game> getGames() {
+        return Collections.unmodifiableList(games);
+    }
 }
